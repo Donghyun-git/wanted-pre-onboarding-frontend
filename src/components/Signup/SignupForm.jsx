0bd0d7ecfd344c05ae5d8f18020d59496dc0bf51
donghyun-git <donghyun1113@hanmail.net>
2023-07-30 00:26:50 +0900

    [FEAT]: 로그인 되어있을 시, 페이지 예외처리 구현, 로그인 버튼 disabled

--- src/components/Signup/SignupForm.jsx
+++ src/components/Signup/SignupForm.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
   emailValidator,
@@ -11,8 +11,25 @@ function SignupForm() {
   const navigate = useNavigate();
   const [emailInput, setEmailInput] = useState('');
   const [passwordInput, setPasswordInput] = useState('');
-  const [isValidEmail, setIsValidEmail] = useState(true);
-  const [isValidPassword, setIsValidPassword] = useState(true);
+  const [isValidEmail, setIsValidEmail] = useState(false);
+  const [isValidPassword, setIsValidPassword] = useState(false);
+
+  const [isAccessTokenValid, setIsAccessTokenValid] = useState(false);
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('access_token');
+    if (accessToken) {
+      setIsAccessTokenValid(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isAccessTokenValid) {
+      alert('이미 로그인이 되어있습니다.');
+      navigate('/todo');
+      return;
+    }
+  }, [isAccessTokenValid, navigate]);
 
   const handleChangeEmail = useCallback((e) => {
     const { value } = e.target;
@@ -59,7 +76,9 @@ function SignupForm() {
             data-testid="email-input"
             onChange={(e) => handleChangeEmail(e)}
           />
-          {!isValidEmail && <p>이메일에 '@' 가 포함되어 있어야 합니다.</p>}
+          {emailInput !== '' && !isValidEmail && (
+            <p>이메일에 '@' 가 포함되어 있어야 합니다.</p>
+          )}
         </Styled.InputDiv>
 
         <Styled.InputDiv>
@@ -70,10 +89,16 @@ function SignupForm() {
             id="password"
             onChange={(e) => handleChangePassword(e)}
           />
-          {!isValidPassword && <p>패스워드는 8자리 이상이여야 합니다.</p>}
+          {passwordInput !== '' && !isValidPassword && (
+            <p>패스워드는 8자리 이상이여야 합니다.</p>
+          )}
         </Styled.InputDiv>
         <Styled.SignupButtonDiv>
-          <button data-testid="signup-button" onClick={(e) => handleSignup(e)}>
+          <button
+            data-testid="signup-button"
+            disabled={isValidEmail && isValidPassword ? false : true}
+            onClick={(e) => handleSignup(e)}
+          >
             회원가입
           </button>
         </Styled.SignupButtonDiv>
--- src/components/Signup/Styled.js
+++ src/components/Signup/Styled.js
@@ -40,11 +40,17 @@ export const SignupButtonDiv = styled.div`
     height: 4.286rem;
     border-radius: 0.857rem;
     color: #fff;
-    transition: all 0.1 ease;
+    transition: all 0.1s ease;
 
     &:hover {
       background: #979797;
     }
+  }
+
+  button[disabled] {
+    background-color: #e0e0e0;
+    color: #aaaaaa;
+    cursor: not-allowed;
   }
 
   div {
